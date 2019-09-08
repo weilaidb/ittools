fd4142661ef87fd2ed4d00c2f93b68d57d9cd91a
yjduoduo <wxjlmr@126.com>
2019-09-08 20:59:09 +0800

    添加apk

--- app/src/main/java/com/example/dell/it/MainActivity.java
+++ app/src/main/java/com/example/dell/it/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dell.it;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -20,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonset()
     {
+        //open url
+        handler_button_pub_url(R.id.button_python, "https://www.python.org/");
+        handler_button_pub_url(R.id.button_linux, "https://www.linux.org/");
+        handler_button_pub_url(R.id.button_kernel, "https://www.kernel.org/");
+//        handler_button_pub_url(R.id.button_testfail, "abb");
+
+
+        //open application
         handler_button_pub(R.id.button_weather,"com.moji.mjweather","com.moji.mjweather.MainActivity","手机没有安装墨迹天气");
         handler_button_pub(R.id.button_github,"me.majiajie.mygithub","me.majiajie.mygithub.activities.welcome.WelcomeActivity","手机没有安装GitHub");
         handler_button_pub(R.id.button_search,"com.microsoft.bing","com.microsoft.clients.bing.app.MainActivity","手机没有安装必应");
@@ -35,6 +44,20 @@ public class MainActivity extends AppCompatActivity {
            handler_button_pub(R.id.button_weixin,"com.tencent.mm",
                    "com.tencent.mm.ui.LauncherUI",
                   "手机未安装微信");
+
+           handler_button_pub(R.id.button_riji,"com.youdao.note",
+                   "com.youdao.note.activity2.SplashActivity",
+                  "手机未安装有道云笔记");
+
+           handler_button_pub(R.id.button_caculate,"caculator.bianfl.cn.abccaculator",
+                   "caculator.bianfl.cn.abccaculator.activitys.CaculateActivity",
+                  "手机未安装计算器");
+
+           handler_button_pub(R.id.button_csdn,"net.csdn.csdnplus",
+                   "net.csdn.csdnplus.activity.MainActivity",
+                  "手机未安装CSDN");
+
+
     }
 
     public void handler_button_pub(int id, final String pkgname, final String activityname, final String errortip)
@@ -47,6 +70,26 @@ public class MainActivity extends AppCompatActivity {
                         activityname,
                         errortip
                 );
+            }
+        });
+    }
+    public void handler_button_pub_url(int id, final String surl) {
+        Button btn = (Button) findViewById(id);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    String texttmp = (String) surl;
+                    Uri uri = Uri.parse(texttmp);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+//                MainActivity.this.startActivityForResult(intent, 0x11);
+                } catch (Exception e) {
+                    Toast toast = Toast.makeText(MainActivity.this, "打开网址失败" + surl, Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                }
             }
         });
     }
